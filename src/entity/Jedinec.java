c92dd9977a33a8befa4be1e8b1554f0c2f26d823
Michal Slovík <mishcos@gmail.com>
2017-02-11 11:53:33 +0100

    update

---
+++ src/entity/Jedinec.java
@@ -0,0 +1,45 @@
+package entity;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import logic.InitObject;
+
+public class Jedinec {
+
+	public int[] instrukcie; 	//zoznam instrukcii
+	public Set<Integer> najdenePoklady; //zoznam najdenych pokladov
+	public int fitnessHodnota;
+	//public int pocetNajdenychPokladov;
+	public int pocetVykonanychInstrukcii;
+	public int aktPozicia;
+	public boolean vysielMimoMapy;
+
+	public Jedinec() {
+	}
+
+	public Jedinec(Jedinec jedinec, InitObject init) {
+		this.vysielMimoMapy = false;
+		this.aktPozicia = init.startPoint;
+		instrukcie = new int[64];
+		System.arraycopy(jedinec.instrukcie, 0, this.instrukcie, 0, 64);
+		najdenePoklady = new TreeSet<>();
+	}
+
+	public Jedinec(InitObject init, int[] instrukcia) {
+		this.vysielMimoMapy = false;
+		this.aktPozicia = init.startPoint;
+		this.instrukcie = new int[64];
+		System.arraycopy(instrukcia, 0, this.instrukcie, 0, 64);
+		najdenePoklady = new TreeSet<>();
+	}
+
+	public Jedinec(InitObject init) {
+		this.vysielMimoMapy = false;
+		this.aktPozicia = init.startPoint;
+		instrukcie = new int[64];
+		najdenePoklady = new TreeSet<>();
+
+	}
+
+}
